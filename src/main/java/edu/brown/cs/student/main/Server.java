24982646d84bec4e60ae2cb40f3bacc50e84e3a5
ffnaqvi <faizah_naqvi@brown.edu>
2024-02-09 15:28:26 -0500

    class structure

--- src/main/java/edu/brown/cs/student/main/Server.java
+++ src/main/java/edu/brown/cs/student/main/Server.java
@@ -4,17 +4,17 @@ import edu.brown.cs.student.main.Utility.*;
 import java.io.*;
 
 /** The Main class of our project. This is where execution begins. */
-public final class Main {
+public final class Server {
   /**
    * The initial method called when execution begins.
    *
    * @param args An array of command line arguments
    */
   public static void main(String[] args) throws IOException {
-    new Main(args).run();
+    new Server(args).run();
   }
 
-  private Main(String[] args) {}
+  private Server(String[] args) {}
 
   /**
    * Method to run the program
