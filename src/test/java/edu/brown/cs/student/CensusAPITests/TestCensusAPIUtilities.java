ddfcfb8da681580d2873641ef81f0e26543980d7
ffnaqvi <faizah_naqvi@brown.edu>
2024-02-15 01:25:11 -0500

    feat: testing frameowrk

--- src/test/java/edu/brown/cs/student/CensusAPITests/TestCensusAPIUtilities.java
+++ src/test/java/edu/brown/cs/student/CensusAPITests/TestCensusAPIUtilities.java
@@ -26,15 +26,12 @@ public class TestCensusAPIUtilities {
 
     @Test
     public void testCensusDeserialize() throws IOException {
-        String chickenNoodle =
-                "{\n"
-                        + "    \"soupName\": \"chicken noodle\",\n"
-                        + "    \"ingredients\": [\"chicken broth\", \"celery\", \"carrot\", \"onion\"],\n"
-                        + "    \"isHot\": true\n"
-                        + "  }";
-        List<Census> censusList = CensusAPIUtilities.deserializeCensus(chickenNoodle);
+        String lauderdaleCounty = "[[\"NAME\",\"S2802_C03_022E\",\"state\",\"county\"],\n" +
+                "[\"Lauderdale County, Alabama\",\"77.8\",\"01\",\"077\"]]";
+        List<Census> censusList = CensusAPIUtilities.deserializeCensus(lauderdaleCounty);
         // This might throw an IOException, but if so JUnit will mark the test as failed.
-        Assert.assertTrue(censusList.get(0).equals("hello")); //TODO: replace hello with valid string
+        System.out.println(censusList.get(0).toString());
+        Assert.assertEquals("[Lauderdale County, Alabama has the estimated percent broadband internet subscription of: 77.8%]", censusList.get(0).toString());
     }
 
 }
